85594c7b7698d745d0c869457827d25eff8c0e44
772821140@qq.com <772821140@qq.com>
2017-04-08 21:40:03 +0800

    前台内容清空，初始化状态

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -9,9 +9,9 @@ return array(
 	//'LAYOUT_ON' => true,
 	//'LAYOUT_NAME' => 'layout',
 
-	'SITE_TITLE' =>	'全球购,海外代购,海淘攻略【配夸网官网|peikua.com】',
-	'SITE_KTYWORDS' => '海淘网址,全球购首页, 国外代购网站',
-	'SITE_DESCRIPTION' => '配夸海淘,配夸网peikua.com是一站式海外代购网站,100%正品海外代购网站！解决您如何海淘,怎么海淘的难题！代购商品：母婴,奶粉,化妆品,数码,电器,奢侈品...品牌代购上千个品牌!',
+	'SITE_TITLE' =>	'',
+	'SITE_KTYWORDS' => '',
+	'SITE_DESCRIPTION' => '',
 				
 	
 );
